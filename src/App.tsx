1e635d09b90d0d5fc90563c12f94c32a898b26f6
EmranAli-15 <mohammademranali15@gmail.com>
2024-07-09 10:48:08 +0600

    updated url added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, useState } from "react";
 import { HiClipboardDocumentList, HiEye, HiEyeSlash } from "react-icons/hi2";
+import './App.css';
 
 export default function App() {
   const [hash, setHash] = useState('HASH GENERATOR');
@@ -40,7 +41,7 @@ export default function App() {
     })
       .then((response) => response.json())
       .then((data) => {
-        setHash(data.name)
+        setHash(data.password)
       });
 
     setPassErr("");
@@ -48,26 +49,26 @@ export default function App() {
   }
 
   return (
-    <div className="h-screen w-full bg-[#101820FF] flex flex-col gap-y-20 justify-center items-center pb-[10%]">
+    <div className="h-screen w-full bg-[#101820FF] flex flex-col gap-y-20">
 
-      <div>
-        <p className="text-white text-lg">{passErr}</p>
-        <p className="text-white text-lg">{saltErr}</p>
+      <div className="mt-10">
+        <p className="text-white text-lg text-center font-mono">{passErr}</p>
+        <p className="text-white text-lg text-center font-mono">{saltErr}</p>
       </div>
 
-      <div className="w-[70%] md:w-[600px] mx-auto h-11 rounded-lg outline-none p-3 font-semibold bg-[#006B38FF] flex justify-between items-center">
+      <div className="w-[80%] md:w-[600px] mx-auto h-11 rounded-lg outline-none p-3 font-semibold bg-[#006B38FF] flex justify-between items-center">
         <p className="text-white">{hash}</p>
         <button className="text-white" onClick={copyToClipboard}>
           <HiClipboardDocumentList size={25} />
         </button>
       </div>
 
-      <div className=" w-full flex flex-col justify-center items-center gap-y-5">
+      <div className="flex flex-col justify-center items-center gap-y-5">
 
-        <div className="flex flex-col gap-y-5 w-full md:w-[600px]">
-          <div className="flex justify-center">
-            <input onChange={(e) => setPassword(e.target.value)} className="w-[70%] h-11 rounded-lg border-[3px] outline-none p-3 font-semibold border-[#006B38FF] ml-[23px]" type={eye ? 'password' : 'text'} placeholder="Provide simple password" />
-            <button className="text-[#006B38FF]" onClick={() => (setEye(!eye))}>
+        <div className="w-screen flex flex-col gap-y-5 md:w-[600px]">
+          <div className="w-full flex justify-center relative">
+            <input onChange={(e) => setPassword(e.target.value)} className="input-field" type={eye ? 'password' : 'text'} placeholder="Provide simple password" />
+            <button className="text-[#006B38FF] absolute translate-y-1/2 right-[4%] md:right-[5%]" onClick={() => (setEye(!eye))}>
               {
                 eye ? <HiEyeSlash size={23} /> : <HiEye size={23} />
               }
@@ -75,12 +76,12 @@ export default function App() {
           </div>
 
           <div className="flex justify-center">
-            <input onChange={(e) => setSaltRound(e.target.value)} className="w-[70%] h-11 rounded-lg border-[3px] outline-none p-3 font-semibold border-[#006B38FF]" type="number" placeholder="Select salt-round ( 1 - 30 )" />
+            <input onChange={(e) => setSaltRound(e.target.value)} className="input-field" type="number" placeholder="Select salt-round ( 1 - 30 )" />
           </div>
         </div>
 
         <div>
-          <button onClick={fn} type="submit" className="bg-[#006B38FF] py-2 px-4 rounded-lg font-semibold text-white text-[18px]">
+          <button onClick={fn} type="submit" className="bg-[#006B38FF] py-[6px] px-4 rounded-lg font-semibold text-white text-[18px]">
             GENERATE
           </button>
         </div>
